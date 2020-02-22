8f678b61e2978493e3b0738558f515e3fb8a052e
MagicJson <it_magicjson@163.com>
2020-02-22 15:31:17 +0800

    Site updated: 2020-02-22 15:31:16

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+         $('[rel="icon"]').attr('href', "https://s2.ax1x.com/2020/02/22/3KjrY6.png");
          document.title = '(ノへ￣、)不要离开我';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+         $('[rel="icon"]').attr('href', "https://s2.ax1x.com/2020/02/22/3KjrY6.png");
          document.title = '(￣▽￣)～■干杯□～(￣▽￣) ！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
