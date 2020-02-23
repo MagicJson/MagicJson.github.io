176d1105c12f94c172b743df8cd87128995caf03
MagicJson <it_magicjson@163.com>
2020-02-23 17:37:16 +0800

    Site updated: 2020-02-23 17:37:14

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,11 +1,4 @@
-/*  添加信息到控制面板*/
-	console.log("\n %c MagicJson %c https://magicjson.cc \n",
-		"color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;")
-	console.log(
-		"%c亲爱的朋友: \n  你好呀,很高兴你能看见这段信息,本人为一名普普通通都程序员,\n" +
-		"在这浩如烟海的技术世里,希望我和你可以不忘初心,努力加油,\n" +
-		"如果你想和我交个朋友!一定要联系我哦,谢谢^^!! \n 我的邮箱: it_magicjson@163.com ",
-		"color: #202124; background: #ccffff; padding:6px;")
+
 		
 		
 		<!--浏览器标题-->
